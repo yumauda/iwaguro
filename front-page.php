e4eb8bedf49a585e5ef54ffb2d0863dfd021d05d
yumauda <firststep-yumauda@gmail.com>
2025-08-01 22:35:44 +0900

    最初のコミット

---
+++ front-page.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-mv">
+    <div class="l-inner">
+      <div class="p-mv__content">
+        <figure class="p-mv__img">
+          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/mv_img.jpg" alt="" width="431" height="38">
+        </figure>
+        <div class="p-mv__detail">
+          <h2 class="p-mv__title">
+            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/mv_title.png" alt="あなたの技術は、生きるを支える" width="521" height="148">
+          </h2>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="p-gradation">
+    <div class="l-inner">
+      <div class="p-gradation__content">
+        <div class="p-gradation__text-wrapper">
+          <p class="p-gradation__text">普段、何気なく手に取っている<span class="c-text-gradation">医薬品</span>。<br>
+            そのパッケージ、<br>岩黒製作所が関わっているかもしれません。</p>
+          <p class="p-gradation__text">私たちは ｢限りなき技術への挑戦｣ を信念に<br>
+            医療・製薬業界の発展に貢献し続ける<br>
+            薬剤包装機械製造のソリューション・カンパニー。</p>
+          <p class="p-gradation__text">医薬という、<br>
+            大事な<span class="c-text-gradation">命・健康</span> を支えるものだからこそ<br>
+            それらを包む技術に私たちは情熱をかけています。</p>
+          <p class="p-gradation__text">ただモノづくりをするのではなく、<br>
+            <span class="c-text-gradation">医薬の一つひとつ</span> に、<span class="c-text-gradation">オンリーワンの包み方</span> を<br>
+            ゼロから考え、つくり、安心を届ける。</p>
+          <p class="p-gradation__text">創意は無限、改良は永遠。</p>
+          <p class="p-gradation__text">なくてはならない「くすり」。<br>
+            その一錠、一包が、<br>
+            人々を支える、<span class="c-text-gradation">生きる力</span>になることを信じて。</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="p-top-image">
+    <figure class="p-top-image__img">
+      <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_img1.jpg" alt="" width="480" height="270">
+    </figure>
+    <figure class="p-top-image__img">
+      <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_img2.jpg" alt="" width="480" height="270">
+    </figure>
+    <figure class="p-top-image__img">
+      <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/top_img3.jpg" alt="" width="480" height="270">
+    </figure>
+  </div>
+</main>
+<?php get_footer() ?>
