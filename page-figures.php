f84e256ad45b04ce0d44b649775cf6df16260075
yumauda <firststep-yumauda@gmail.com>
2025-08-09 14:09:32 +0900

    数字で見るページ構築

---
+++ page-figures.php
@@ -0,0 +1,173 @@
+<?php get_header(); ?>
+<main>
+  <section class="p-page-mv">
+    <div class="l-inner">
+      <div class="p-page-mv__content">
+        <div class="p-page-mv__detail p-page-mv__detail--figures">
+          <ul class="p-page-mv__breadcrumbs">
+            <li class="p-page-mv__breadcrumbs-list">
+              <a href="<?php echo esc_url(home_url('/')); ?>" class="p-page-mv__breadcrumbs-link">top</a>
+            <li class="p-page-mv__breadcrumbs-list">
+              <span class="p-page-mv__breadcrumbs-link">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/breadcrumbs_arrow.svg" alt="" width="10" height="10">
+              </span>
+            <li class="p-page-mv__breadcrumbs-list">
+              <p class="p-page-mv__breadcrumbs-link">数字で見る岩黒製作所</p>
+            </li>
+          </ul>
+          <div class="p-page-mv__title-wrapper">
+            <p class="c-page-en">FIGURES</p>
+            <h2 class="p-page-mv__title">
+              <span class="c-page-ja">数字で見る岩黒製作所</span>
+            </h2>
+          </div>
+          <div class="p-page-mv__text-wrapper">
+            <p class="p-page-mv__text">
+              岩黒製作所をさまざまな角度から数字で紐解きます。
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-figures mt80">
+    <div class="l-inner">
+      <div class="p-figures__content">
+        <div class="p-figures__top">
+          <div class="p-figures__title-wrapper">
+            <figure class="p-figures__icon">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_icon1.png" alt="会社について" width="130" height="130">
+            </figure>
+            <h3 class="p-figures__title">会社について</h3>
+          </div>
+          <p class="p-figures__number">DATE 01</p>
+        </div>
+        <div class="p-figures__block">
+
+          <ul class="p-figures__triple">
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img1.jpg" alt="" width="360" height="184">
+              </figure>
+            </li>
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img2.jpg" alt="" width="360" height="184">
+              </figure>
+            </li>
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img3.jpg" alt="" width="360" height="184">
+              </figure>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-figures">
+    <div class="l-inner">
+      <div class="p-figures__content">
+        <div class="p-figures__top">
+          <div class="p-figures__title-wrapper">
+            <figure class="p-figures__icon">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_icon2.png" alt="働き方について" width="130" height="130">
+            </figure>
+            <h3 class="p-figures__title">働き方について</h3>
+          </div>
+          <p class="p-figures__number">DATE 02</p>
+        </div>
+        <div class="p-figures__block">
+          <p class="p-figures__year">※令和6年度</p>
+          <div class="p-figures__double mt20">
+            <ul class="p-figures__lists">
+              <li class="p-figures__list">
+                <figure class="p-figures__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img4.jpg" alt="平均年収" width="545" height="289">
+                </figure>
+              </li>
+              <li class="p-figures__list">
+                <figure class="p-figures__img">
+                  <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img5.jpg" alt="年間休日" width="545" height="289">
+                </figure>
+              </li>
+
+            </ul>
+          </div>
+
+          <ul class="p-figures__triple mt10">
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img6.jpg" alt="平均有給休暇取得日数" width="360" height="184">
+              </figure>
+            </li>
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img7.jpg" alt="平均時間外労働時間" width="360" height="184">
+              </figure>
+            </li>
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img8.jpg" alt="育児休業取得率" width="360" height="184">
+              </figure>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="p-figures">
+    <div class="l-inner">
+      <div class="p-figures__content">
+        <div class="p-figures__top">
+          <div class="p-figures__title-wrapper">
+            <figure class="p-figures__icon">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_icon3.png" alt="社員について" width="130" height="130">
+            </figure>
+            <h3 class="p-figures__title">社員について</h3>
+          </div>
+          <p class="p-figures__number">DATE 03</p>
+        </div>
+        <div class="p-figures__block">
+          <p class="p-figures__year">※令和6年度</p>
+          <ul class="p-figures__triple mt20">
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img9.jpg" alt="従業員数" width="360" height="184">
+              </figure>
+            </li>
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img10.jpg" alt="従業員男女比" width="360" height="184">
+              </figure>
+            </li>
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img11.jpg" alt="職種別人員比率" width="360" height="184">
+              </figure>
+            </li>
+          </ul>
+          <ul class="p-figures__triple mt10">
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img12.jpg" alt="過去3年間の定着率" width="360" height="184">
+              </figure>
+            </li>
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img13.jpg" alt="平均勤続年数" width="360" height="184">
+              </figure>
+            </li>
+            <li class="p-figures__triple-list">
+              <figure class="p-figures__img">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/figure_img14.jpg" alt="最年少・最年長" width="360" height="184">
+              </figure>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+</main>
+<?php get_footer() ?>
