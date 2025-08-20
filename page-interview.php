da1ecbe4ed2d0c30ed5a3ece41ac7156438031ec
yumauda <firststep-yumauda@gmail.com>
2025-08-20 11:03:17 +0900

    interviewページSP化

--- page-interview.php
+++ page-interview.php
@@ -4,14 +4,7 @@
     <div class="l-inner l-single-inner">
       <div class="p-page-mv__content">
         <div class="p-page-mv__detail p-page-mv__detail--figures">
-
-          <div class="p-page-mv__title-wrapper">
-            <p class="c-page-en--ja">仲間を知る</p>
-            <h2 class="p-page-mv__title">
-              <span class="c-page-ja--en">INTERVIEW</span>
-            </h2>
-          </div>
-          <ul class="p-page-mv__breadcrumbs p-page-mv__breadcrumbs--interview">
+          <ul class="p-page-mv__breadcrumbs p-page-mv__breadcrumbs--interview p-page-mv__breadcrumbs--interviewSp">
             <li class="p-page-mv__breadcrumbs-list">
               <a href="<?php echo esc_url(home_url('/')); ?>" class="p-page-mv__breadcrumbs-link">top</a>
             </li>
@@ -21,7 +14,34 @@
               </span>
             </li>
             <li class="p-page-mv__breadcrumbs-list">
-              <p class="p-page-mv__breadcrumbs-link">数字で見る岩黒製作所</p>
+              <p class="p-page-mv__breadcrumbs-link">仲間を知る</p>
+            </li>
+            <li class="p-page-mv__breadcrumbs-list">
+              <span class="p-page-mv__breadcrumbs-link">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/breadcrumbs_arrow.svg" alt="" width="10" height="10">
+              </span>
+            </li>
+            <li class="p-page-mv__breadcrumbs-list">
+              <p class="p-page-mv__breadcrumbs-link">M.T.</p>
+            </li>
+          </ul>
+          <div class="p-page-mv__title-wrapper">
+            <p class="c-page-en--ja">仲間を知る</p>
+            <h2 class="p-page-mv__title">
+              <span class="c-page-ja--en">INTERVIEW</span>
+            </h2>
+          </div>
+          <ul class="p-page-mv__breadcrumbs p-page-mv__breadcrumbs--interview p-page-mv__breadcrumbs--interviewPc">
+            <li class="p-page-mv__breadcrumbs-list">
+              <a href="<?php echo esc_url(home_url('/')); ?>" class="p-page-mv__breadcrumbs-link">top</a>
+            </li>
+            <li class="p-page-mv__breadcrumbs-list">
+              <span class="p-page-mv__breadcrumbs-link">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/breadcrumbs_arrow.svg" alt="" width="10" height="10">
+              </span>
+            </li>
+            <li class="p-page-mv__breadcrumbs-list">
+              <p class="p-page-mv__breadcrumbs-link">仲間を知る</p>
             </li>
             <li class="p-page-mv__breadcrumbs-list">
               <span class="p-page-mv__breadcrumbs-link">
@@ -40,7 +60,10 @@
     <div class="l-inner l-single-inner">
       <div class="p-single__content">
         <figure class="p-single__img">
-          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/single_img_top.jpg" alt="" width="1000" height="1000">
+          <picture>
+            <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/single_img_top.jpg" media="(min-width: 768px)" width="1238" height="570" />
+            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/single_img_top_sp.jpg" alt="" width="355" height="227">
+          </picture>
         </figure>
         <div class="p-single__detail">
           <div class="p-single__number">
@@ -72,14 +95,18 @@
         <div class="p-question__block">
           <h4 class="p-question__q">
             <span class="p-question__q--en">Q</span>
-            <p class="p-question__title">機械設計から制御設計に変わり、苦労したことは？</p>
+            <p class="p-question__title">機械設計から制御設計に変わり、<br class="u-mobile">苦労したことは？</p>
           </h4>
           <div class="p-question__answer-wrapper">
             <p class="p-question__answer">大学は機械科で基本的な知識はありましたが、同じ設計でも機械を「つくる」のと「動かす」のでは全く異なり、最初は思うようにいかず苦労しました。わからないところを積極的に聞いたり、独学で勉強するうちに、思い通りに動かせるようになってきて。システムによって色々な動きをつくり出せるのが面白く、この仕事に引き込まれていきました。社内でも機械設計と制御設計の両方の経験がある人は少ないので、自分の強みだと感じています。</p>
           </div>
         </div>
         <figure class="p-question__img">
-          <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/question_img1.jpg" alt="" width="1100" height="473">
+          <picture>
+            <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/question_img1.jpg" media="(min-width: 768px)" width="1238" height="570" />
+            <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri(); ?>/images/common/question_img1_sp.jpg" alt="" width="355" height="227">
+          </picture>
+          <!-- <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/question_img1.jpg" alt="" width="1100" height="473"> -->
         </figure>
         <div class="p-question__row-wrapper">
 
@@ -110,25 +137,29 @@
               </div>
             </div>
             <figure class="p-question__rowImg">
-              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/question_img2.jpg" alt="" width="431" height="380">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/question_img3.jpg" alt="" width="431" height="380">
             </figure>
           </div>
         </div>
         <div class="p-question__who">
           <div class="p-question__who-detail">
             <p class="p-question__who-en">Who is suitable for Iwakuro?</p>
+
             <div class="p-question__who-titleWrapper">
               <h4 class="p-question__q">
                 <span class="p-question__q--en p-question__q--white">Q</span>
                 <p class="p-question__title p-question__title--white">岩黒製作所に向いている人は？</p>
               </h4>
+              <figure class="p-question__who-img u-mobile">
+                <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/question_white_img1.png" alt="" width="300" height="380">
+              </figure>
               <div class="p-question__white">
-                <p class="p-question__white-text"><span class="c-text-gradation">機械が好き、モノづくりがしたい人</span> に向いている職場だと思います。<br>
-                  機械について専門的に学んだことがない人でも仕事を通して成長できるので、<span class="c-text-gradation">チャレンジ精神や学ぶ意欲があれば大丈夫！</span> やりがいのある仕事なので、少しでも興味があればぜひ挑戦してほしいです。</p>
+                <p class="p-question__white-text"><span>機械が好き、モノづくりがしたい人</span> に向いている職場だと思います。<br class="u-desktop">
+                  機械について専門的に学んだことがない人でも仕事を通して成長できるので、<span>チャレンジ精神や学ぶ意欲があれば大丈夫！</span> やりがいのある仕事なので、少しでも興味があればぜひ挑戦してほしいです。</p>
               </div>
             </div>
           </div>
-          <figure class="p-question__who-img">
+          <figure class="p-question__who-img u-desktop">
             <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/question_white_img1.png" alt="" width="300" height="380">
           </figure>
         </div>
@@ -138,13 +169,17 @@
   <section class="p-other">
     <div class="l-inner l-inner--interview">
       <div class="p-other__content">
-        <h3 class="p-other__title">other interview</h3>
+        <h3 class="p-other__title">other <br class="u-mobile">interview</h3>
         <div class="swiper swiper1">
           <!-- Additional required wrapper -->
           <div class="swiper-wrapper">
             <!-- Slides -->
             <div class="swiper-slide">
               <a href="#" class="p-top-interview__slider-card">
+                <div class="p-top-interview__num-wrapper">
+                  <p class="p-top-interview__no">no.</p>
+                  <p class="p-top-interview__num">01</p>
+                </div>
                 <figure class="p-top-interview__slider-img">
                   <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/interview_img1.png" alt="" width="270" height="270">
                 </figure>
@@ -167,6 +202,10 @@
             </div>
             <div class="swiper-slide">
               <a href="#" class="p-top-interview__slider-card">
+                <div class="p-top-interview__num-wrapper">
+                  <p class="p-top-interview__no">no.</p>
+                  <p class="p-top-interview__num">02</p>
+                </div>
                 <figure class="p-top-interview__slider-img">
                   <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/interview_img1.png" alt="" width="270" height="270">
                 </figure>
@@ -189,6 +228,10 @@
             </div>
             <div class="swiper-slide">
               <a href="#" class="p-top-interview__slider-card">
+                <div class="p-top-interview__num-wrapper">
+                  <p class="p-top-interview__no">no.</p>
+                  <p class="p-top-interview__num">03</p>
+                </div>
                 <figure class="p-top-interview__slider-img">
                   <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/interview_img1.png" alt="" width="270" height="270">
                 </figure>
@@ -230,6 +273,16 @@
             <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/next.png" alt="" width="54" height="54">
           </figure>
         </div>
+      </div>
+      <div class="p-pickup__btn">
+        <a href="" class="c-btn">
+          <p class="c-btn__text">view more</p>
+          <span class="c-btn__arrow">
+            <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path class="c-btn__path" d="M16.3464 11.6291H13.2691C13.1885 11.632 13.1083 11.6186 13.033 11.5897C12.9578 11.5609 12.8892 11.5172 12.8312 11.4612C12.7732 11.4052 12.7271 11.3382 12.6956 11.264C12.6641 11.1898 12.6479 11.1101 12.6479 11.0295C12.6479 10.9489 12.6641 10.8692 12.6956 10.795C12.7271 10.7208 12.7732 10.6538 12.8312 10.5978C12.8892 10.5419 12.9578 10.4982 13.033 10.4693C13.1083 10.4404 13.1885 10.427 13.2691 10.4299L17.7946 10.4299C17.9536 10.43 18.106 10.4932 18.2184 10.6056C18.3309 10.718 18.3941 10.8705 18.3942 11.0295L18.3942 15.555C18.397 15.6355 18.3836 15.7158 18.3548 15.791C18.3259 15.8662 18.2822 15.9349 18.2262 15.9929C18.1703 16.0508 18.1032 16.0969 18.029 16.1284C17.9549 16.1599 17.8751 16.1762 17.7946 16.1762C17.714 16.1762 17.6342 16.1599 17.5601 16.1284C17.4859 16.0969 17.4188 16.0508 17.3629 15.9929C17.3069 15.9349 17.2632 15.8662 17.2343 15.791C17.2055 15.7158 17.1921 15.6355 17.1949 15.555L17.1949 12.4777L11.7134 17.9592C11.6009 18.0717 11.4483 18.1349 11.2892 18.1349C11.13 18.1349 10.9774 18.0717 10.8649 17.9592C10.7524 17.8466 10.6892 17.694 10.6892 17.5349C10.6892 17.3758 10.7524 17.2231 10.8649 17.1106L16.3464 11.6291Z" fill="white" />
+            </svg>
+          </span>
+        </a>
       </div>
     </div>
   </section>
