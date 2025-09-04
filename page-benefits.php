19ae3e65669913e0d59caf0f5d59df5b46403fae
yumauda <firststep-yumauda@gmail.com>
2025-09-04 16:05:15 +0900

    福利厚生制度pc

--- page-benefits.php
+++ page-benefits.php
@@ -38,7 +38,204 @@
       </div>
     </div>
   </section>
+  <section class="p-benefits">
+    <div class="l-inner">
+      <div class="p-benefits__content">
+        <div class="p-benefits__title">
+          <div class="c-benefits-title">
+            <p class="c-benefits-title__en">EMPLOYEE BENEFITS</p>
+            <h3 class="c-benefits-title__ja">福利厚生について</h3>
+          </div>
+        </div>
+        <ul class="p-benefits__lists">
+          <li class="p-benefits__list">
+            <div class="p-benefits__number">
+              <p class="p-benefits__number-en">benefits</p>
+              <p class="p-benefits__number-num">01</p>
+            </div>
+            <p class="p-benefits__list-title">転勤なし｜年間休日<span>122</span>日（土日完全休業）</p>
+            <p class="p-benefits__list-text">年間<span>122</span>日の休日があります。転勤もなく、仕事とプライベートのメリハリをつけて働けます。</p>
+          </li>
+          <li class="p-benefits__list">
+            <div class="p-benefits__number">
+              <p class="p-benefits__number-en">benefits</p>
+              <p class="p-benefits__number-num">02</p>
+            </div>
+            <p class="p-benefits__list-title">決算賞与 支給あり（<span>15</span>年連続）</p>
+            <p class="p-benefits__list-text">年2回の賞与に加えて、業績に応じて期末決算賞与を支給しています。期末決算賞与は【<span>15</span>年連続】支給中です。</p>
+          </li>
+          <li class="p-benefits__list">
+            <div class="p-benefits__number">
+              <p class="p-benefits__number-en">benefits</p>
+              <p class="p-benefits__number-num">03</p>
+            </div>
+            <p class="p-benefits__list-title">手厚い出張手当</p>
+            <p class="p-benefits__list-text">国内出張時の手当として最大1万円/泊を支給いたしております。(他社にはない手厚い出張手当は弊社のユニークな福利厚生です。)</p>
+          </li>
+          <li class="p-benefits__list">
+            <div class="p-benefits__number">
+              <p class="p-benefits__number-en">benefits</p>
+              <p class="p-benefits__number-num">04</p>
+            </div>
+            <p class="p-benefits__list-title">昼食代の補助</p>
+            <p class="p-benefits__list-text">昼食代（お弁当）の一部補助を行っています。</p>
+          </li>
+          <li class="p-benefits__list">
+            <div class="p-benefits__number">
+              <p class="p-benefits__number-en">benefits</p>
+              <p class="p-benefits__number-num">05</p>
+            </div>
+            <p class="p-benefits__list-title">資格取得支援制度あり</p>
+            <p class="p-benefits__list-text">“業務に資する”と会社が認めた資格は、取得費用全額を補助しています。</p>
+          </li>
+          <li class="p-benefits__list">
+            <div class="p-benefits__number">
+              <p class="p-benefits__number-en">benefits</p>
+              <p class="p-benefits__number-num">06</p>
+            </div>
+            <p class="p-benefits__list-title">社員旅行</p>
+            <p class="p-benefits__list-text">社員旅行の一部費用を補助しています。過去には、沖縄、ハワイ、上海、台湾などさまざまな地域を訪問しました。</p>
+          </li>
+          <li class="p-benefits__list">
+            <div class="p-benefits__number">
+              <p class="p-benefits__number-en">benefits</p>
+              <p class="p-benefits__number-num">07</p>
+            </div>
+            <p class="p-benefits__list-title">社員預金制度あり</p>
+            <p class="p-benefits__list-text">希望者のみ、最大<span>500</span>万円まで年利<span>3%</span>で運用可能な社員預金制度を利用いただけます。</p>
+          </li>
+          <li class="p-benefits__list">
+            <div class="p-benefits__number">
+              <p class="p-benefits__number-en">benefits</p>
+              <p class="p-benefits__number-num">08</p>
+            </div>
+            <p class="p-benefits__list-title">退職金制度（企業型<span>DC</span>）あり</p>
+            <p class="p-benefits__list-text">退職一時金のほか、企業型<span>DC</span>、中小企業退職金共済制度があります。</p>
+          </li>
+          <li class="p-benefits__list">
+            <div class="p-benefits__number">
+              <p class="p-benefits__number-en">benefits</p>
+              <p class="p-benefits__number-num">09</p>
+            </div>
+            <p class="p-benefits__list-title">保険料会社負担で病気やケガ等のリスクに備え</p>
+            <p class="p-benefits__list-text">入社と同時に“総合団体定期保険”、<span>1</span>年後に“養老保険”に加入。</p>
+          </li>
+          <li class="p-benefits__list">
+            <div class="p-benefits__number">
+              <p class="p-benefits__number-en">benefits</p>
+              <p class="p-benefits__number-num">10</p>
+            </div>
+            <p class="p-benefits__list-title">人間ドック受診料補助</p>
+            <p class="p-benefits__list-text">会社補助により、割安で人間ドックを受診できます。</p>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  <figure class="p-section-img">
+    <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/section_img.jpg" alt="" width="1440" height="397">
+  </figure>
+  <section class="p-career">
+    <div class="l-inner">
+      <div class="p-career__content">
+        <div class="p-career__title">
+          <div class="c-benefits-title">
+            <p class="c-benefits-title__en">CAREER</p>
+            <h3 class="c-benefits-title__ja">キャリアパス</h3>
+          </div>
+        </div>
+        <div class="p-career__text-wrapper">
+          <p class="p-career__text">社員が安心して働くことができ、挑戦し続けられる環境が整っています。<br>
+            現場では知識や経験の豊富なベテラン社員が責任をもって基本からしっかり教えます。<br>
+            分からないことは一緒に解決し、技術を習得いただけるよう日々の環境づくりにも取り組んでいます。</p>
+        </div>
+        <div class="p-career__block-wrapper">
+          <div class="p-career__block">
+            <div class="p-career__block-title">
+              <div class="c-benefits-title">
+                <p class="c-benefits-title__en">CAREER PATH OPTIONS</p>
+                <h3 class="c-benefits-title__ja c-benefits-title__ja--small">キャリアパスの選択肢</h3>
+              </div>
+            </div>
+            <figure class="p-career__block-img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/career_path_img.png" alt="キャリアパスの選択" width="1021" height="396">
+            </figure>
+          </div>
+          <div class="p-career__block">
+            <div class="p-career__block-title">
+              <div class="c-benefits-title">
+                <p class="c-benefits-title__en">WORK STEPS</p>
+                <h3 class="c-benefits-title__ja c-benefits-title__ja--small">仕事のステップ</h3>
+              </div>
+            </div>
+            <ul class="p-career__steps-lists">
+              <li class="p-career__steps-list">
+                <div class="p-career__steps-top">
+                  <div class="p-career__steps-num">
+                    <p class="p-career__steps-list-title">STEP</p>
+                    <p class="p-career__steps-list-num">01</p>
+                  </div>
+                  <p class="p-career__steps-title">現場で仕事を覚える（<span>3</span>年〜）</p>
+                  <div class="p-career__steps-line"></div>
+                </div>
+                <div class="p-career__steps-detail">
+                  <p class="p-career__steps-important">重要</p>
+                  <p class="p-career__steps-elect">仕上げ/電気</p>
+                </div>
+              </li>
+              <li class="p-career__steps-list">
+                <div class="p-career__steps-top">
+                  <div class="p-career__steps-num">
+                    <p class="p-career__steps-list-title">STEP</p>
+                    <p class="p-career__steps-list-num">02</p>
+                  </div>
+                  <p class="p-career__steps-title">プロフェッショナル人材へ</p>
+                  <div class="p-career__steps-line"></div>
+                </div>
+                <ul class="p-career__lists">
+                  <li class="p-career__list">
+                    <p class="p-career__list-title">仕上げ/電気</p>
+                    <p class="p-career__list-text">プロフェッショナル/責任者を目指す</p>
+                  </li>
+                  <li class="p-career__list">
+                    <p class="p-career__list-title">設計</p>
+                    <p class="p-career__list-text">機械設計・制御設計・プロフェッショナルを目指す</p>
+                  </li>
+                  <li class="p-career__list">
+                    <p class="p-career__list-title">営業</p>
+                    <p class="p-career__list-text">お客様の課題解決提案・プロフェッショナルを目指す</p>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <figure class="p-career__bottom-img">
+              <img decoding="async" loading="lazy" src="<?php echo get_template_directory_uri() ?>/images/common/career_environment.jpg" alt="仕事のステップ" width="963" height="410">
+            </figure>
+          </div>
+          <div class="p-career__block">
+            <div class="p-career__block-title">
+              <div class="c-benefits-title">
+                <p class="c-benefits-title__en">QUALIFICATION</p>
+                <h3 class="c-benefits-title__ja c-benefits-title__ja--small">取得できる資格 (従業員が保有している資格)</h3>
+              </div>
+            </div>
+            <ul class="p-career__qualification-lists">
+              <li class="p-career__qualification-list">電気主任技術者</li>
+              <li class="p-career__qualification-list">電気工事士</li>
+              <li class="p-career__qualification-list">危険物取扱者</li>
+              <li class="p-career__qualification-list">危険物取扱主任者</li>
+              <li class="p-career__qualification-list">第一種衛生管理者</li>
+              <li class="p-career__qualification-list">金属塗装一級技能検定</li>
+              <li class="p-career__qualification-list">エネルギー(電気)管理士</li>
+              <li class="p-career__qualification-list">玉掛技能講習</li>
+              <li class="p-career__qualification-list">クレーン運転業務特別教育</li>
+              <li class="p-career__qualification-list">床上操作式クレーン運転技能講習</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 
-  
 </main>
 <?php get_footer() ?>
